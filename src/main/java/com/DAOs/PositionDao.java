c7f318cf3187eb9879711db4e3d51cb55b2b88af
FANGH <FANGH8768@uwec.edu>
2020-09-21 20:31:15 +0200

    fix merge conflict 5

--- src/main/java/com/DAOs/PositionDao.java
+++ src/main/java/com/DAOs/PositionDao.java
@@ -1,11 +1,11 @@
 package com.DAOs;
 
+import com.Entities.Department;
 import com.Entities.Position;
+import com.Entities.User;
 
 import java.util.List;
 
 public interface PositionDAO extends DAO<Position>{
-
-    public List<Position> getPositionsByDepartement (Integer id);
-
+    public List<Position> getPositionsByDepartement(Integer id);
 }
