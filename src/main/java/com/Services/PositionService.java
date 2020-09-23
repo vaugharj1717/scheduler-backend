a0e6a0d36ead5c0453d47240f1567023fee19944
vaugharj1717 <vaugharj1717@gmail.com>
2020-09-23 11:06:08 -0500

    Add endpoint for assigning a candidate to a position, returning a Candidacy JSON object.

--- src/main/java/com/Services/PositionService.java
+++ src/main/java/com/Services/PositionService.java
@@ -1,9 +1,11 @@
 package com.Services;
 
+import com.Entities.Candidacy;
 import com.Entities.Position;
 
 import java.util.List;
 
 public interface PositionService {
     public List<Position> getPositionsByDepartment (Integer id);
+    public Candidacy assignCandidateToPosition(Integer positionId, Integer candidateId);
 }
--- src/main/java/com/Services/PositionServiceImpl.java
+++ src/main/java/com/Services/PositionServiceImpl.java
@@ -1,7 +1,11 @@
 package com.Services;
 
+import com.DAOs.CandidacyDAO;
 import com.DAOs.PositionDAO;
+import com.DAOs.UserDAO;
+import com.Entities.Candidacy;
 import com.Entities.Position;
+import com.Entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,11 +17,27 @@ public class PositionServiceImpl implements PositionService{
 
     @Autowired
     PositionDAO positionDao;
+    @Autowired
+    UserDAO userDao;
+    @Autowired
+    CandidacyDAO candidacyDao;
 
     @Override
     @Transactional
     public List<Position> getPositionsByDepartment(Integer id) {
         List<Position> positionList = positionDao.getPositionsByDepartment(id);
         return positionList;
+    }
+
+    @Override
+    @Transactional
+    public Candidacy assignCandidateToPosition(Integer positionId, Integer candidateId){
+        Position position = positionDao.getById(positionId);
+        User candidate = userDao.getById(candidateId);
+        Candidacy newCandidacy = new Candidacy();
+        newCandidacy.setCandidate(candidate);
+        newCandidacy.setPosition(position);
+        return candidacyDao.saveOrUpdate(newCandidacy);
+
     }
 }
