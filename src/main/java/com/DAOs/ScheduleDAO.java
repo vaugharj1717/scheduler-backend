4874625bca39db17511e5f53a3f986350ee2294c
NicoleMcMahon <mcmahond2895@uwec.edu>
2020-09-22 23:00:16 -0500

    update remove schedule

--- src/main/java/com/DAOs/ScheduleDAO.java
+++ src/main/java/com/DAOs/ScheduleDAO.java
@@ -3,4 +3,7 @@ package com.DAOs;
 import com.Entities.Schedule;
 
 public interface ScheduleDAO extends DAO<Schedule>{
+    public Schedule getById(Integer id);
+    public Schedule saveOrUpdate(Schedule schedule);
+    public void remove(Integer id);
 }
