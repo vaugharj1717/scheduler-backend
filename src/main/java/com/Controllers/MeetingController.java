e3dd77306e4de25dab61cf20f3b60f9e845e1590
NicoleMcMahon <mcmahond2895@uwec.edu>
2020-10-08 16:46:56 -0500

    Start adding meetings to schedules

--- src/main/java/com/Controllers/MeetingController.java
+++ src/main/java/com/Controllers/MeetingController.java
@@ -1,12 +1,19 @@
 package com.Controllers;
 
 import com.Entities.Meeting;
+import com.Entities.Participation;
 import com.Services.MeetingService;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 @CrossOrigin
 @RestController()
@@ -39,6 +46,18 @@ public class MeetingController {
             Integer locationId = body.get("locationId").asInt();
             String startTime = body.get("startTime").asText();
             String endTime = body.get("endTime").asText();
+            ArrayNode getParticipants =  (ArrayNode) body.get("participants");
+            Iterator<JsonNode> itr = getParticipants.elements();
+
+            List<Participation> participationList = new ArrayList<Participation>();
+            while(itr.hasNext()) {
+                JsonNode nextNode = itr.next();
+                Participation participation = new Participation();
+                participation.setCanLeaveFeedback(nextNode.get("canLeaveFeedback").asBoolean());
+                participation.setCanViewFeedback(nextNode.get("canViewFeedback").asBoolean());
+
+            }
+
             Meeting newMeeting = meetingService.createMeeting(scheduleId, locationId, startTime, endTime);
             if(newMeeting == null) return new ResponseEntity<Meeting>(HttpStatus.BAD_REQUEST);
             else{
@@ -61,4 +80,6 @@ public class MeetingController {
             return new ResponseEntity<Meeting>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+
 }
